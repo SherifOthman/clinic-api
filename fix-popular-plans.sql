21d55cc0e3f8fb041eefdfc226b0429526b2dc7d
Sherif Ali <elemlak202099@gmail.com>
2026-03-20 21:10:30 +0200

    Add Docker configuration for all services

--- fix-popular-plans.sql
+++
@@ -1,8 +0,0 @@
--- Fix duplicate popular plans - only Professional should be popular
-UPDATE SubscriptionPlans 
-SET IsPopular = 0 
-WHERE Name = 'Basic';
-
-UPDATE SubscriptionPlans 
-SET IsPopular = 1 
-WHERE Name = 'Professional';
