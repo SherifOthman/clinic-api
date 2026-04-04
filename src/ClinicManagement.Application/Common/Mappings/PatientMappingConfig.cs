604ebbe83ab8a8cac9b9ba32209f06eef2812189
Clinic Dev <dev@clinicmanagement.com>
2026-04-05 01:27:52 +0200

    fix: blood type display in Mapster mapping config, add ToBloodTypeDisplay helper

--- src/ClinicManagement.Application/Common/Mappings/PatientMappingConfig.cs
+++ src/ClinicManagement.Application/Common/Mappings/PatientMappingConfig.cs
@@ -1,18 +1,27 @@
 using ClinicManagement.Application.Features.Patients.Queries;
 using ClinicManagement.Domain.Entities;
+using ClinicManagement.Domain.Enums;
 using Mapster;
 
 namespace ClinicManagement.Application.Common.Mappings;
 
 public class PatientMappingConfig : IRegister
 {
+    private static string? ToBloodTypeDisplay(BloodType bt) => bt switch
+    {
+        BloodType.APositive  => "A+",  BloodType.ANegative  => "A-",
+        BloodType.BPositive  => "B+",  BloodType.BNegative  => "B-",
+        BloodType.ABPositive => "AB+", BloodType.ABNegative => "AB-",
+        BloodType.OPositive  => "O+",  BloodType.ONegative  => "O-",
+        _ => null,
+    };
     public void Register(TypeAdapterConfig config)
     {
         config.NewConfig<Patient, PatientDto>()
             .Map(dest => dest.Id, src => src.Id.ToString())
             .Map(dest => dest.DateOfBirth, src => src.DateOfBirth.ToString("yyyy-MM-dd"))
             .Map(dest => dest.Age, src => DateTime.UtcNow.Year - src.DateOfBirth.Year)
-            .Map(dest => dest.BloodType, src => src.BloodType.HasValue ? src.BloodType.Value.ToString() : null)
+            .Map(dest => dest.BloodType, src => src.BloodType.HasValue ? ToBloodTypeDisplay(src.BloodType.Value) : null)
             .Map(dest => dest.PrimaryPhone, src =>
                 src.Phones.Where(ph => ph.IsPrimary).Select(ph => ph.PhoneNumber).FirstOrDefault()
                 ?? src.Phones.Select(ph => ph.PhoneNumber).FirstOrDefault())
